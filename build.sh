95b4bc0da7627982fe70fd0740a68b1be80a06d1
Artem Vetrov <tsunamaru@airmail.cc>
2022-09-10 14:43:53 +0400

    Bump gradle to 7.5.1

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-docker run -it --rm -v "$(pwd)":"$(pwd)" -w "$(pwd)" gradle:7.4.2-jdk17 gradle assemble --stacktrace --debug
+docker run -it --rm -v "$(pwd)":"$(pwd)" -w "$(pwd)" gradle:7.5.1-jdk17 gradle assemble --stacktrace --debug
 docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/tsunamaru/spodlivoibot/ci-build:latest .
